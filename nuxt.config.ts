8646aa76b41f6b0c5425c4080883fb93f90423de
cschweda <cschweda@gmail.com>
2025-11-26 19:31:00 -0600

    fix: use yarn generate for static site deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,9 @@ export default defineNuxtConfig({
       routes: ["/", "/sitemap.xml", "/robots.txt"],
       ignore: ["/admin"],
     },
+    output: {
+      dir: "dist",
+    },
   },
 
   modules: [
